206b8b0d3dee2d642f7aec4bf52646424b6d2c30
Josephus Paye II <j.paye96@gmail.com>
2021-03-13 18:06:37 +1100

    Add help text for shortcuts

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -29,8 +29,19 @@
           d="M16 10c0 0.553-0.048 1-0.601 1h-4.399v4.399c0 0.552-0.447 0.601-1 0.601s-1-0.049-1-0.601v-4.399h-4.399c-0.552 0-0.601-0.447-0.601-1s0.049-1 0.601-1h4.399v-4.399c0-0.553 0.447-0.601 1-0.601s1 0.048 1 0.601v4.399h4.399c0.553 0 0.601 0.447 0.601 1z"
         ></path>
       </svg>
-      <div class="ml-1">Add another</div>
+      <div class="ml-1">
+        {{ lowerThirds.length === 0 ? 'Add new' : 'Add another' }}
+      </div>
     </button>
+    <div
+      class="mt-4 text-sm text-gray-400 hover:text-gray-900 focus:text-gray-900 text-center px-2 pb-2 transition-colors duration-300"
+      tabindex="0"
+    >
+      Shortcuts: select a slide by clicking on its edge, then press:
+      <kbd>Delete</kbd> to remove, <kbd>d</kbd> to duplicate, <kbd>↑</kbd> to
+      move up, <kbd>↓</kbd> to move down, <kbd>b</kbd> to insert new slide
+      before, <kbd>a</kbd> to insert new slide after
+    </div>
   </div>
 </template>
 
@@ -45,3 +56,9 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+kbd {
+  font-weight: bold;
+}
+</style>
